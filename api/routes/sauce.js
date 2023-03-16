643fc37fa24d7c1ca6455f381a64458f3ac0aedf
Eline31 <eline.chambon@gmail.com>
2023-03-16 15:53:35 +0100

    Création architecture API

--- api/routes/sauce.js
+++ api/routes/sauce.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 const user = require("../models/user");
-
+const sauce = require("../models/sauce");
 
 module.exports = router;
---
+++ api/routes/user.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+
+const user = require("../models/user");
+const sauce = require("../models/sauce");
+
+module.exports = router;
