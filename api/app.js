ead9de61031bf917ed431d439b8339b99312c904
Eline31 <eline.chambon@gmail.com>
2023-03-16 11:54:37 +0100

    connexion api à mongoDB réussie

--- api/app.js
+++ api/app.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://Piiquante_API:pyzMrvQgGdjHgziB@cluster0.2ol13jn.mongodb.net/?retryWrites=true&w=majority",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log("Connexion à MongDB réussie !"))
+    .catch(() => console.log("Connexion à MongoDB échouée !"));
 /**Middleware pour extraire le corps JSON afin de gérer la requête post */
 app.use(express.json());
 
