14e581d2fae4936b3b11f022ce0eb3b40fb88cd0
Eline31 <eline.chambon@gmail.com>
2023-03-16 11:02:54 +0100

    ajout d'un middleware express

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-
+/**Middleware pour extraire le corps JSON afin de gérer la requête post */
+app.use(express.json());
 
 // const userSchema = require("./models/user");
 app.use((req, res) => {
