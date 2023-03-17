4f041d73b47dc9823f4f60b14c076e59c2ffc4ed
Eline31 <eline.chambon@gmail.com>
2023-03-17 10:33:02 +0100

    Installation et mise en place validator

--- api/models/user.js
+++ api/models/user.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 /**Création d'un schéma de données contenant les champs souhaités */
 const userSchema = mongoose.Schema({
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
 });
 
+userSchema.plugin(uniqueValidator);
+
 /**On exporte le schéma en tant que modèle mongoose le rendant dispo pour l'application Express */
-module.exports = mongoose.model("Utilisateur", userSchema);
+module.exports = mongoose.model("User", userSchema);
